4322bd4295ad9ec21193be2aa51fa8bbbe8be1c6
Pitacd <jappita15@hotmail.com>
2023-11-19 22:56:10 +0000

    Change function putPieceOnTheBoard to increase variable in brain.py
    Change function goBackToInitialPosition

--- movement.py
+++ movement.py
@@ -1,5 +1,6 @@
 from pybricks.tools import wait
-from brain import *
+from pybricks.parameters import Color
+import brain
 
 # Function to make the robot go to the corret position to 
 # put the piece on the board depending on the line and column
@@ -14,7 +15,7 @@ def goToPositionOnBoard(line, column, robot, colorSensor):
     robot.drive(100, 0)
     
     while numberOfBlackLinesPassed < numberOfBlackLinesToPutPiece:
-        wait(90)
+        wait(100)
         if colorSensor.color() == Color.BLACK:
             numberOfBlackLinesPassed += 1
             
@@ -22,15 +23,26 @@ def goToPositionOnBoard(line, column, robot, colorSensor):
 
 # function of the movement of the robot right before 
 # arrived on the position to put the piece on the board 
-def putPieceOnTheBoard(distanceToComeBack, robot, rotationMotor):
+def putPieceOnTheBoard(robot, rotationMotor):
     robot.straight(150)
     wait(200)
     robot.turn(-90)
     wait(100)
     robot.straight(400)
     rotationMotor.run_angle(100, 90)
-    robot.straight(150)
+    brain.numberPiecesOnBoard += 1
+    robot.straight(200)
+    wait(200)
+    robot.turn(-90)
+    
+# function to make the robot come back right after it puts 
+# the piece on the board. Note this needs a box in the 
+# beginning to this works
+def goBackToInitialPosition(distanceToComeBack, robot, ultrasonSensor):
+    robot.straight(distanceToComeBack)
     wait(200)
     robot.turn(-90)
     wait(100)
-    robot.straight(distanceToComeBack) 
+    robot.straight(ultrasonSensor.distance() - 10) # change if box moved from the tests
+    wait(200)
+    robot.turn(-90)
