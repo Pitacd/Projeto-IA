dbc5a882dac00d88e804c1b46f12ae579e2acfe6
Antonio2088114 <148369818+Antonio2088114@users.noreply.github.com>
2023-12-28 13:12:13 +0000

    peçasfora

--- heuristica.py
+++ heuristica.py
@@ -17,7 +17,7 @@ class Tabuleiro:
     def __str__(self):
         tab = ""
         for Position in self.tab:
-            tab += str(Position)
+            tab += str(Position) + "\n"
         return tab
     
 t1= Tabuleiro()
@@ -26,5 +26,41 @@ t1.criarTabuleiroInicial()
 
 print(t1)
 
-
+class PiecesOutSide:
+    def __init__(self,listOutSide):
+        self.arrayListOutSide = listOutSide
+        self.possibleForms = {"X":(0,0),"O":(0,0,0,0),"-":(0,0),"+":(0,0)}
+        
+    def nPiecesOutSide(self):
+        npieces = {"X": 0, "+": 0, "-": 0, "O": 0}
+        for piece in self.arrayListOutSide:
+            npieces[piece] += 1
+        return npieces
+    
+    def possibleForms(self):
+        forms = self.nPiecesOutSide()
+        for piece in forms:
+            if forms[piece] > 0:
+                if piece == "X" or piece == "+":
+                    self.arrayListOutSide[piece][0] = forms[piece] / 9
+                    self.arrayListOutSide[piece][1] = forms[piece] / 5
+                elif piece == "-":
+                    self.arrayListOutSide[piece][0] = forms[piece] / 3
+                    self.arrayListOutSide[piece][1] = forms[piece] / 2
+                elif piece == "O":
+                    self.arrayListOutSide[piece][0] = forms[piece] / 16
+                    self.arrayListOutSide[piece][1] = forms[piece] / 12
+                    self.arrayListOutSide[piece][2] = forms[piece] / 8
+                    self.arrayListOutSide[piece][3] = forms[piece] / 4
+    
+                    
+                    
+                    
+                    
+                
+                    
+                    
+                
+            
+        
     
