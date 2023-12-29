e48041d22a5566288cf6237afbb695625db534cd
Pitacd <jappita15@hotmail.com>
2023-12-29 14:46:08 +0000

    Functions to get all the possible set of combination for the board given by the number of pieces

--- heuristicFunction_2/reservePositionForFormClass.py
+++ heuristicFunction_2/reservePositionForFormClass.py
@@ -1,4 +1,20 @@
 class reservePositionsOnBoard:
+    def __init__(self):
+        # list that contains all the possible combinations
+        # to form the shape of a small minus
+        self.listPossibleSetMinusReservationSmallShape = [((line, column), (line, column + 1)) for line in range(5) for column in range(4)]
+        # list that contains all the possible combinations
+        # to form the shape of a big minus
+        self.listPossibleSetMinusReservationBigShape = [((line,column),(line,column+1),(line,column+2)) for line in range(5) for column in range(3)]
+        # list that contains all the possible combinations 
+        # to form the shape of a small plus (left,up,center,right,down)
+        self.listPossibleSetPlusReservationSmallShape = [((i,j-1),(i-1,j),(i, j),(i,j+1),(i+1,j)) for i in range(1, 4) for j in range(1, 4)]
+        # list that contains all the possible combinations 
+        # to form the shape of a big plus 
+        # [down+, up, left, left+, down+, right, up+, center, down]
+        self.listPossiblePlusReservationBigShape = [tuple(set([(2, j) for j in range(5)] + [(i, 2) for i in range(5)]))]
+        
+        self.listPossibleSetXReservationSmallShape = [((i-1,j-1),(i-1,j+1),(i, j),(i+1,j+1),(i+1,j-1)) for i in range(1, 4) for j in range(1, 4)]
     # def listSetPositionForForm(self, piece, typeForm):
     #     match piece:
     #         case "-":
@@ -8,35 +24,63 @@ class reservePositionsOnBoard:
     #         case "O":
                 
     #         case "X":
-    
-    def setPositionForMinusForm(typeForm, board):
+    #TODO change the above functions to receive the position of the piece put on the board and give its possible reservations to make a shape
+     
+    def setPositionForMinusForm(self, numberOfPieces, board):
         listPossibleSetMinusReservation = []
         
-        if typeForm == 0:
-            for line in range(5):
-                for column in range(4):
-                    listPossibleSetMinusReservation.append(((line, column),(line, column+1)))
+        if numberOfPieces >= 2:
+            # copy of the list that contains all the possible combinations
+            # to form the shape of a small minus
+            listPossibleSetMinusReservation += self.listPossibleSetMinusReservationSmallShape.copy()
             
-            for possibleSetReservation in listPossibleSetMinusReservation.copy():
-                if board[possibleSetReservation[0][0]*5 + possibleSetReservation[0][1]].positionIsEmpty():
-                    if not board[possibleSetReservation[1][0]*5 + possibleSetReservation[1][1]].positionIsEmpty():
+            for possibleSetReservation in self.listPossibleSetMinusReservationSmallShape:
+                for position in possibleSetReservation:
+                    if not board[position[0]*5 + position[1]].positionIsEmpty():
                         listPossibleSetMinusReservation.remove(possibleSetReservation)
-                else:
-                    listPossibleSetMinusReservation.remove(possibleSetReservation)
-        else:
-            for line in range(5):
-                for column in range(3):
-                    listPossibleSetMinusReservation(((line,column),(line,column+1),(line,column+2)))
+                        break
+        if numberOfPieces >= 3:
+            # copy of the list that contains all the possible combinations
+            # to form the shape of a big minus
+            listPossibleSetMinusReservation += self.listPossibleSetMinusReservationBigShape.copy()
             
-            for possibleSetReservation in listPossibleSetMinusReservation.copy():
-                if board[possibleSetReservation[0][0]*5 + possibleSetReservation[0][1]].positionIsEmpty():
-                    if board[possibleSetReservation[1][0]*5 + possibleSetReservation[1][1]].positionIsEmpty():
-                        if not board[possibleSetReservation[1][0]*5 + possibleSetReservation[1][1]].positionIsEmpty():
-                            listPossibleSetMinusReservation.remove(possibleSetReservation)
-                    else:
+            for possibleSetReservation in self.listPossibleSetMinusReservationBigShape:
+                for position in possibleSetReservation:
+                    if not board[position[0]*5 + position[1]].positionIsEmpty():
                         listPossibleSetMinusReservation.remove(possibleSetReservation)
-                else:
-                    listPossibleSetMinusReservation.remove(possibleSetReservation)
+                        break
                     
         return listPossibleSetMinusReservation
+        
+    def setPositionForPlusForm(self, numberOfPieces, board):
+        listPossibleSetPlusReservation = []
+        
+        if numberOfPieces >= 5:
+            # copy of list that contains all the possible combinations 
+            # to form the shape of a small plus (left,up,center,right,down)
+            listPossibleSetPlusReservation += self.listPossibleSetPlusReservationSmallShape.copy()
+            
+            for possibleSetReservation in self.listPossibleSetPlusReservationSmallShape:
+                for position in possibleSetReservation:
+                    if not  board[position[0]*5 + position[1]].positionIsEmpty():
+                        listPossibleSetPlusReservation.remove(possibleSetReservation)
+                        break
+        if numberOfPieces >= 9:
+            # list that contains all the possible combinations 
+            # to form the shape of a big plus 
+            # [down+, up, left, left+, down+, right, up+, center, down]
+            listPossibleSetPlusReservation = self.listPossibleSetPlusReservationBigShape.copy()
+            
+            for possibleSetReservation in self.listPossibleSetPlusReservationBigShape:
+                for position in possibleSetReservation:
+                    if not  board[position[0]*5 + position[1]].positionIsEmpty():
+                        listPossibleSetPlusReservation.remove(possibleSetReservation)
+                        break
+        
+        return listPossibleSetPlusReservation
+    
+    # def setPositionForXForm(self,typeForm, board):
+    #     listPossibleSetXReservation = []
+        
+    #     if typeForm == 0:
             
