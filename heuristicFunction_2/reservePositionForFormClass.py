f4f911781dcdf39543592672948985b82493084c
Pitacd <jappita15@hotmail.com>
2023-12-30 11:09:32 +0000

    Add comments

--- heuristicFunction_2/reservePositionForFormClass.py
+++ heuristicFunction_2/reservePositionForFormClass.py
@@ -1,5 +1,8 @@
 class reservePositionsOnBoard:
     def __init__(self):
+        # dictionary that has an tuple with the number of pieces of 
+        # each type of piece to form the shapes tinny, big, mega and ultra 
+        # (in this sequence)
         self.numberPieceNeedToShape = {'-' : (2,3), '+' : (5,9), 'X' : (5,9), 'O' : (4,8,12,16)}
         # list that contains all the possible combinations
         # to form the shape of a small minus
@@ -34,9 +37,16 @@ class reservePositionsOnBoard:
         # to form the shape of a mega circle
         self.listPossibleCircleReservationMegaShape = [tuple(set([(i,j) for i in range(5) for j in (0,4)] + [(i,j) for i in (0,4) for j in range(5)]))]
     
-    #TODO change the above functions to receive the position of the piece put on the board and give its possible reservations to make a shape
-    
     def listSmallShapeToUse(self, piece):
+        """
+        The function returns a list of possible small shapes to use based on the given piece.
+        
+        Arguments:
+            piece : a char, one of the following values: '-', '+', 'X', or 'O'
+            
+        Returns:
+            listOfPossibleSmallShapes: a list
+        """
         match piece:
                 case '-':
                     return self.listPossibleSetMinusReservationSmallShape
@@ -48,6 +58,15 @@ class reservePositionsOnBoard:
                     return self.listPossibleSetCircleReservationSmallShape
     
     def listBigShapeToUse(self,piece):
+        """
+        The function returns a list of possible big shapes to use based on the given piece.
+        
+        Arguments:
+            piece : a char, one of the following values: '-', '+', 'X', or 'O'
+            
+        Returns:
+            listOfPossibleBigShapes: a list
+        """
         match piece:
             case '-':
                 return self.listPossibleSetMinusReservationBigShape
@@ -59,6 +78,15 @@ class reservePositionsOnBoard:
                 return self.listPossibleSetCircleReservationMidShape
     
     def possibleFormToDoInTheBoard(self, board, listPossibleFormToDo, listPossibleSetReservationShape):
+        """
+        The function checks if a list of possible reservations can be made on a board by removing any
+        reservations that have occupied positions.
+        
+        Arguments:
+            board: a matrix 5x5
+            listPossibleFormToDo: a list 
+            listPossibleSetReservationShape: a list
+        """
         for possibleSetReservation in listPossibleSetReservationShape:
                 for position in possibleSetReservation:
                     if not board[position[0]*5 + position[1]].positionIsEmpty():
@@ -66,6 +94,15 @@ class reservePositionsOnBoard:
                         break
     
     def listSetPositionForPieceForm(self, numberOfPieces, piece, board):
+        """
+        The function returns a list of possible set positions for a given
+        piece on a board, based on the number of available pieces and the shape of the piece.
+        
+        Arguments:
+            numberOfPieces: an integer
+            piece: a char
+            board: a matrix 5x5
+        """
         listPossibleSetReservation = []
         numberPiecePerShape = self.numberPieceNeedToShape[piece]
         
