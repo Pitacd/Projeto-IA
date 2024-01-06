13235533355724d5f598ffc7d70ee03bdcecb8be
Pitacd <jappita15@hotmail.com>
2024-01-06 19:33:23 +0000

    Change name of variable

--- heuristicFunction_2/heuristic2.py
+++ heuristicFunction_2/heuristic2.py
@@ -4,15 +4,16 @@ from reservePositionBoard import *
 from brain import listPiecesOutside
 from removePiece import removeForms
 
-class node:
+class Node:
     def __init__(self, parent, board, piecesOutside, piece = '_', position = None):
         self.board = board
         self.piecesOutside = piecesOutside
         self.costValue = piecesOutside.getNumberPiecesRemoved() + board.valuePositionForPieceOnBoard(piece, position)
         self.heuristicValue = board.diffReservedPositLapReservedPosit()
-        self.positionPut = position
+        self.positionPlaced = position
         self.parent = parent
-        
+
+
 
 # create the frontier
 frontier = []
@@ -22,9 +23,9 @@ startBoard = Board()
 startBoard.createInitialBoard()
 # initialize the list with pieces outside
 # given by robot, read it by the sensor 
-startLisPiecesOutside = PiecesOutSide(listPiecesOutside)
+startPiecesOutside = PiecesOutSide(listPiecesOutside)
+# create the first node
+root = Node(None, startBoard, startPiecesOutside)
 
-valueOfThisInitialNode = startLisPiecesOutside.getNumberPiecesRemoved() + startBoard.diffReservedPositLapReservedPosit() + startBoard.valuePositionForPieceOnBoard()
-
-
+print(root)
 
