fb7fba9fb17c2c0c237b71d87f2cc588ac66578e
Pitacd <jappita15@hotmail.com>
2024-01-06 19:19:41 +0000

    Add class node

--- heuristicFunction_2/heuristic2.py
+++ heuristicFunction_2/heuristic2.py
@@ -4,6 +4,16 @@ from reservePositionBoard import *
 from brain import listPiecesOutside
 from removePiece import removeForms
 
+class node:
+    def __init__(self, parent, board, piecesOutside, piece = '_', position = None):
+        self.board = board
+        self.piecesOutside = piecesOutside
+        self.costValue = piecesOutside.getNumberPiecesRemoved() + board.valuePositionForPieceOnBoard(piece, position)
+        self.heuristicValue = board.diffReservedPositLapReservedPosit()
+        self.positionPut = position
+        self.parent = parent
+        
+
 # create the frontier
 frontier = []
 
@@ -14,6 +24,7 @@ startBoard.createInitialBoard()
 # given by robot, read it by the sensor 
 startLisPiecesOutside = PiecesOutSide(listPiecesOutside)
 
-valueOfThisInitialNode = startLisPiecesOutside.getNumberPiecesRemoved() + startBoard.diffReservedPositLapReservedPosit() #+ startBoard.valuePositionForPieceOnBoard()
+valueOfThisInitialNode = startLisPiecesOutside.getNumberPiecesRemoved() + startBoard.diffReservedPositLapReservedPosit() + startBoard.valuePositionForPieceOnBoard()
+
 
 
