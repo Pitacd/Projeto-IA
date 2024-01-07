d464e283ffaa296ecdb14d20316f22c30460acd7
Pitacd <jappita15@hotmail.com>
2024-01-07 23:43:55 +0000

    Test Errors

--- boardClass.py
+++ boardClass.py
@@ -8,11 +8,11 @@ class Board:
         The constructor for the class and initializes a board that is empty.
         """
         self.board = board
-        
+
     def __deepcopy__(self, memo):
         new_instance = self.__class__(copy.deepcopy(self.board, memo))
         return new_instance
-    
+
     def createInitialBoard(self):
         """
         The function creates an initial board by appending positions to a list.
@@ -20,7 +20,7 @@ class Board:
         for line in range(5):
             for column in range(5):
                self.board.append(Position((line,column)))
-               
+
     def boardAsAnMatrix(self):
         """
         The function converts a board represented as a list into a matrix
@@ -35,40 +35,40 @@ class Board:
             if len(boardLineList) > 4:
                 matrixBoard.append(boardLineList.copy())
                 boardLineList.clear()
-        
+
         return matrixBoard
-    
+
     def emptyPositions(self):
         """
         The function returns a list of positions on the board that are empty.
-        
+
         Returns:
             listEmptyPositions: a list of tuples, (line, column)
         """
-        listEmptyPositions = [] 
+        listEmptyPositions = []
         for position in self.board:
             if position.isEmpty():
                 listEmptyPositions.append(position.position)
-        
+
         return listEmptyPositions
-    
+
     def positionOnBoard(self, position):
         """
         The function calculates the position on a board based on the given position coordinates.
-        
+
         Arguments:
             position : (line,column)
-        
+
         Returns:
-            An integer 
+            An integer
         """
         return position[0] * 5 + position[1]
-    
+
     def removePieceFormBoard(self, piece, matrix):
         """
         The function removes a specified shape piece from a board, given by the matrix 5x5
-        result of that removed, by setting its points to 0 and updating the board it self. 
-        
+        result of that removed, by setting its points to 0 and updating the board it self.
+
         Arguments:
             piece: a char
             matrix: a matrix 5x5
@@ -76,28 +76,28 @@ class Board:
         for line in range(5):
             for column in range(5):
                 positionBoard = self.positionOnBoard((line,column))
-                
+
                 if self.board[positionBoard].piece == piece:
                     if matrix[line][column] == '_':
                         self.board[positionBoard].points[piece] = 0
                         self.board[positionBoard].piece = '_'
-    
+
     def putPieceOnTheBoard(self, position, piece):
         """
         The function puts a piece on the board at a specified position and resets its points.
-        
+
         Arguments:
             position: (line, column)
             piece: a char
         """
         indexBoard = self.positionOnBoard(position)
         self.board[indexBoard].piece = piece
-    
+
     def putPointsOnThePositions(self, piece: chr, pattern: list[tuple[int]]):
         """
         The function assigns points to positions on a board based on the given
         piece and pattern.
-        
+
         Arguments:
             piece: a char
             pattern: a list of tuples, where each tuple represents a position on the board
@@ -114,8 +114,8 @@ class Board:
             else:
                 for position in pattern:
                     self.board[self.positionOnBoard(position)].points[piece] = points
-            
-        elif piece == '+' or piece == 'X':
+
+        if piece == '+' or piece == 'X':
             if len(pattern) > 5:
                 for position in pattern:
                     self.board[self.positionOnBoard(position)].points[piece] = points
@@ -123,19 +123,19 @@ class Board:
                         self.board[self.positionOnBoard(position)].points[piece] = points-1
             else:
                 for position in pattern:
-                    self.board[self.positionOnBoard(position)].points[piece] = points 
+                    self.board[self.positionOnBoard(position)].points[piece] = points
 
-        elif piece == 'O':
+        if piece == 'O':
             for position in pattern:
                     self.board[self.positionOnBoard(position)].points[piece] = points
-    
+
     def reservationForPiece(self, piece):
         """
         The function checks if a given piece has a reservation on the board.
-        
-        Arguments: 
+
+        Arguments:
             piece: a char
-        
+
         Returns:
             pieceHasReservation: a boolean
         """
@@ -148,11 +148,11 @@ class Board:
     def valuePositionForPieceOnBoard(self, piece, position):
         """
         The function returns the value of a position on the board for a given piece.
-        
+
         Arguments:
             piece: a char
             position: a (line,column)
-            
+
         Returns:
             valuePositionForPiece: an integer
         """
@@ -160,17 +160,17 @@ class Board:
             return 0
         else:
             return self.board[self.positionOnBoard(position)].valuePositionForPiece(piece)
-    
+
     def diffReservedPositLapReservedPosit(self):
         """
         The function calculates the difference between the number of reserved positions and the number
         of positions that have overlapping reservations on a board.
-        
+
         Returns:
             The difference between the number of reserved positions and the number of positions
         that have a superposed reservation.
         """
-        
+
         positionsReserved = 0
         positionsSuperpose = 0
         for position in self.board:
@@ -178,24 +178,24 @@ class Board:
                 positionsReserved += 1
                 if position.positionIsSuperposeReservation():
                     positionsSuperpose += 1
-        
+
         return positionsReserved - positionsSuperpose
-    
+
     def allPossibleBoardPiecePlace(self,numberOfPieces, piece, position):
         """
         The function returns a list of all possible boards that can be
         created by placing a given number of pieces on a board at a given position.
-        
+
         Arguments:
             numberOsPieces: a integer
             piece: a char
             position: a (line,column)
-            
+
         Returns:
             listPossibleBoards: a list of Boards
         """
-        
-        listPossibleBoards = [] 
+
+        listPossibleBoards = []
         if not self.reservationForPiece(piece):
             currentBoard = self.boardAsAnMatrix()
             listSetPossibleShape = listSetPositionForPieceForm(numberOfPieces, piece, position, currentBoard)
@@ -204,7 +204,8 @@ class Board:
                     newBoard = copy.deepcopy(self)
                     newBoard.putPointsOnThePositions(piece, reservation)
                     newBoard.putPieceOnTheBoard(position,piece)
-                    listPossibleBoards.append(newBoard)
+                    listPossibleBoards.append(copy.deepcopy(newBoard))
+                    newBoard = None
             else:
                 newBoard = copy.deepcopy(self)
                 newBoard.putPieceOnTheBoard(position,piece)
@@ -213,9 +214,9 @@ class Board:
             newBoard = copy.deepcopy(self)
             newBoard.putPieceOnTheBoard(position,piece)
             listPossibleBoards.append(newBoard)
-        
+
         return listPossibleBoards
-            
+
     def __str__(self):
         board = ""
         for Position in self.board:
@@ -230,5 +231,5 @@ class Board:
 #     print(son)
 #     print('------------------------------')
 
-#TODO add a tree class and node class to make the IA 
-#TODO make a function to see if there is a reserved position to that type of piece, case there is put it there if not try to create a new shape, if empty put in an empty spot 
+#TODO add a tree class and node class to make the IA
+#TODO make a function to see if there is a reserved position to that type of piece, case there is put it there if not try to create a new shape, if empty put in an empty spot
